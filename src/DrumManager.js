53dfca0d126741e262d581c894f7ef104c79f387
Zouzhi Nizar <nzz.coding@gmail.com>
2021-03-05 09:31:01 +0100

    implemented the record functionnality without replay

--- src/DrumManager.js
+++ src/DrumManager.js
@@ -20,20 +20,43 @@ class Manager extends React.Component {
         };
         this.banks = [bank1, bank2];
         this.playSound = this.playSound.bind(this);
+        this.switchRecord = this.switchRecord.bind(this);
         this.addTrack = this.addTrack.bind(this);
         this.selectTrack = this.selectTrack.bind(this);
     }
 
     playSound(event) {
-        
+        const audio_url = event.target.dataset.audio_url;
+        const audio = new Audio(audio_url);
+        audio.volume = (this.state.volume/100).toFixed(2);
+        audio.play();
+
+        if (this.state.record) {
+            const sound_info = {
+                audio_url: audio_url,
+                time: Date.now()
+            };
+            this.setState((state) => ({currentRec: state.currentRec.concat(sound_info)}));
+        }
     }
 
     switchRecord(event) {
-
+        if (this.state.currentRec.length > 0 && !this.state.record) {
+            //new record started, override current record
+            this.setState((state) => ({currentRec: []}));
+        }
+        this.setState((state) => ({record: !state.record}));
     }
 
     addTrack(event) {
-
+        if (this.state.currentRec.length > 0) {
+            this.setState((state) => (
+                {
+                    tracks: state.tracks.concat([state.currentRec]),
+                    currentRec: []
+                })
+            );
+        }
     }
 
     selectTrack(event) {
@@ -67,6 +90,14 @@ class Manager extends React.Component {
                     )
                 )
             }
+            <Recorder 
+                record={this.state.record}
+                switchRecord={this.switchRecord}
+                addTrack={this.addTrack}
+                selectTrack={this.selectTrack}
+                currentTrack={this.state.currentTrack} />
+            record: {this.state.currentRec.length}<br />
+            number of tracks: {this.state.tracks.length}
         </div>
         )
     }
--- src/Recorder.js
+++ src/Recorder.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 class Recorder extends React.Component {
     render() {
+        const record_msg = (this.props.record) ? "stop" : "start" //the msg to be prompted in button
         return (
-            <div>this is the recorder</div>
+            <div>
+                this is the recorder
+                <button
+                    className="record-btn"
+                    onClick={this.props.switchRecord}>
+                        {record_msg} recording
+                </button>
+                <button
+                    className="add-track-btn"
+                    onClick={this.props.addTrack}>
+                        add the recorded track
+                </button>
+            </div>
         )
     }
 }
